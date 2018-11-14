26cdf6ed1325d56878ee54cbcf42be8a229cb124
Kenny Liu <kennyqqr@gmail.com>
2018-11-14 13:50:29 +0800

    string practice

---
+++ str.py
@@ -0,0 +1,12 @@
+msg = "{0} Before we begin, let's narrow down what we mean by \"a Python interpreter\"."
+msgw = "temp "
+msgbreak = "\n\r"
+# print(msg)
+#print(msg.find("let"))
+# print(msg.format("First of all."))
+# print(msg.index("let"))
+# print(msgw.isalnum())
+# print(msg.isnumeric())
+# print(msgw.isalpha())
+# print(msgbreak.join("join me"))
+print(msgbreak.join(msg.format(msgw)))
