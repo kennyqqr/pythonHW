cb80d23e6f3a6fdf90927af259a8b179a71bb3f2
Kenny Liu <kennyqqr@gmail.com>
2018-11-14 11:43:15 +0800

    first python file and settings

---
+++ list.py
@@ -0,0 +1,7 @@
+double_digit_evens = [e*2 for e in range(1,14)]
+double_digit_odds = {e*2+1 for e in range(5, 50)}
+dic_digit = {e: e*10 for e in range(1, 11)}
+
+print('evens:' + str(double_digit_evens))
+print('odds:' + str(double_digit_odds))
+print('dictionary:' + str(dic_digit))
